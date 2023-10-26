61122d064c7b2dab365f283e729efc1fb26bd41e
19SERGIO <sergiolondono55@gmail.com>
2023-10-25 23:29:10 -0500

    solucion error importacion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import  ReactDOM  from "react-dom/client";
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
